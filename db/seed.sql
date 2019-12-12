1914cc2c58403b499aab92019e16f6eb098b8279
Hibah Shafi <hibahshafi.hs@gmail.com>
2019-12-11 20:32:18 -0600

    added the files, added the create functionliaty, and the update functionality, need to upload hamburger image file, and fix css

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Supreme', false);
